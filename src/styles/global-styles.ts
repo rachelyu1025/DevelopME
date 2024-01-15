3cb1ba33a1a56c8dece84c095843cf410a2a6e40
rachelyu1025 <rachelyu.1025@gmail.com>
2024-01-15 17:42:13 +0900

    🎨  [structure] src>style> 전역스타일링을 위한 파일들 생성

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,31 @@
+import reset from 'styled-reset'
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+${reset}
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* * {
+        box-sizing: border-box;
+      }
+      body{
+        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+      }
+
+      @media only screen and (max-width: 768px) {
+        body {
+          font-size: 12px;
+        }
+      }
+
+      @media only screen and (max-width: 576px) {
+        body {
+          font-size: 10px;
+        }
+      } */
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+
+}
+
+export type ColorsTypes = typeof colors;
+
+export const theme: DefaultTheme = {
+  colors,
+}
