d72a6fb3fa022dcb7a7f4658f08a2b41eb23ef97
rachelyu1025 <rachelyu.1025@gmail.com>
2024-01-16 15:21:56 +0900

    ✨ [feat] SignUp 페이지 route 추가 생성

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import App from '../App';
-import { Login } from '../Pages/Login';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import App from '../App'
+import { Login } from '../Pages/Login'
+import { SignUp } from '../Pages/SignUp'
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/signIn" element={<Login />} />
+      <Route path="/signUp" element={<SignUp />} />
+      {/* <Route path="/resetPw" element={< />} /> */}
     </Routes>
-  );
-};
+  )
+}
 
-export default Router;
+export default Router
