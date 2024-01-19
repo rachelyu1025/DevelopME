4c52ce8754e264f6b607e1be5e2c85a254e0d76c
EUNBI <rachelyu.1025@gmail.com>
2024-01-19 18:29:13 +0900

    ✨ [feat] 회원가입 페이지 구현

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+import { SignUpComp } from './SignUpComp'
 
 export const SignUp = (): JSX.Element => {
-  return <></>
+  return (
+    <Container>
+      <SignUpComp />
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: #9c8dbe;
+`
