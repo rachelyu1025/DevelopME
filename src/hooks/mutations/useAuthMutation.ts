32c6b7d07f227b96897ffec81f3c2cb932471c2a
EUNBI <rachelyu.1025@gmail.com>
2024-01-24 20:28:23 +0900

    ✨ [feat] 로그인 api 요청 로직 수정 및 성공 시 리다이렉트 적용

--- src/hooks/mutations/useAuthMutation.ts
+++ src/hooks/mutations/useAuthMutation.ts
@@ -3,21 +3,25 @@ import { useSetRecoilState } from 'recoil';
 import { postSignIn } from '../../utils/services/auth';
 import { tokenState } from '../../recoil/atoms/authState';
 import { setCookie } from '../../utils/cookies';
+import { useNavigate } from 'react-router-dom';
 
 const useAuthQuery = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate()
 
   const setTokens = useSetRecoilState(tokenState);
 
   const { mutate: postSignInMutation } = useMutation(postSignIn, {
     onSuccess: res => {
-      const tokens = res.data.token;
-      setCookie('accessToken', tokens.access);
-      setCookie('refreshToken', tokens.refresh);
+      const tokens = res.data;
+      setCookie('accessToken', tokens.accessToken);
+      setCookie('refreshToken', tokens.refreshToken);
 
       setTokens(() => tokens);
 
-      alert('Login Successed!');
+      // redirect to home
+      navigate('/')
+
       queryClient.invalidateQueries('auth_signIn');
     },
   });
